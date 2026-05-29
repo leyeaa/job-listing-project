694e38e114494e11437f046e5a3b17545d7fef08
leyeaa <leyeaa@gmail.com>
2026-05-29 13:35:55 -0230

    fix: app router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+import { useCallback, useMemo } from "react";
 
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
@@ -25,55 +26,71 @@ import type { JobFormInput } from "./types/job";
 const App = () => {
   const { user } = useAuth();
 
-  const addJob = async (newJob: JobFormInput) => {
-    if (!user) {
-      throw new Error("You must be signed in to post a job.");
-    }
+  const addJob = useCallback(
+    async (newJob: JobFormInput) => {
+      if (!user) {
+        throw new Error("You must be signed in to post a job.");
+      }
 
-    await createJob(newJob, user.id);
-  };
-
-  const updateJobById = async (jobId: string, job: JobFormInput) => {
-    if (!user) {
-      throw new Error("You must be signed in to update a job.");
-    }
-
-    await updateJob(jobId, job, user.id);
-  };
-
-  const deleteJob = async (jobId: string) => {
-    if (!user) {
-      throw new Error("You must be signed in to delete a job.");
-    }
-
-    await deleteJobById(jobId, user.id);
-  };
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route element={<ProtectedRoute />}>
-          <Route
-            path="/add-job"
-            element={<AddJobPage addJobSubmit={addJob} />}
-          />
-          <Route
-            path="/edit-job/:id"
-            element={<EditJobPage updateJobSubmit={updateJobById} />}
-          />
-        </Route>
-
-        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>,
-    ),
-    {
-      basename: import.meta.env.BASE_URL,
+      await createJob(newJob, user.id);
     },
+    [user],
+  );
+
+  const updateJobById = useCallback(
+    async (jobId: string, job: JobFormInput) => {
+      if (!user) {
+        throw new Error("You must be signed in to update a job.");
+      }
+
+      await updateJob(jobId, job, user.id);
+    },
+    [user],
+  );
+
+  const deleteJob = useCallback(
+    async (jobId: string) => {
+      if (!user) {
+        throw new Error("You must be signed in to delete a job.");
+      }
+
+      await deleteJobById(jobId, user.id);
+    },
+    [user],
+  );
+
+  const router = useMemo(
+    () =>
+      createBrowserRouter(
+        createRoutesFromElements(
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/jobs" element={<JobsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route element={<ProtectedRoute />}>
+              <Route
+                path="/add-job"
+                element={<AddJobPage addJobSubmit={addJob} />}
+              />
+              <Route
+                path="/edit-job/:id"
+                element={<EditJobPage updateJobSubmit={updateJobById} />}
+              />
+            </Route>
+
+            <Route
+              path="/jobs/:id"
+              element={<JobPage deleteJob={deleteJob} />}
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>,
+        ),
+        {
+          basename: import.meta.env.BASE_URL,
+        },
+      ),
+    [addJob, updateJobById, deleteJob],
   );
 
   return <RouterProvider router={router} />;
